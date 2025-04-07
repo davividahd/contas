31112447c98bb7b0b7da6c40118f1383ae817fc3
davividahd <daviricardosantos2007@gmail.com>
2025-04-07 14:41:56 -0300

    conta.js

--- contas/module/conta.js
+++ contas/module/conta.js
@@ -1,3 +1,4 @@
+import { buscarcontas, contas } from "./titular";
 
 class Conta {
     constructor(agencia, numero, saldo, numeroConta, titular,) {
@@ -11,17 +12,54 @@ class Conta {
     traferir(conta1, conta2) { }
 
     saque(valor) { }
-    
+
     deposito(valor) { }
 
     investimento(valor) { }
 
     extrato(saldo) { }
 
-    autenticar(numeroConta,agencia,senha) { }
+    autenticar(numeroConta, agencia, senha) { let Contas = buscarcontas } buscarcontas(titulares) {
+        console.log(buscarcontas)
+    }
+
+
+
 
     cobrarTaxa(saldo) { }
 
+
 }
 
-export { Conta };
+function buscarcontas() {
+    let c1 = new Conta(
+        11,
+        1,
+        1000,
+        1,
+        titulares[0]
+    )
+
+    let c2 = new Conta(
+        11,
+        2,
+        2000,
+        2,
+        titulares[1]
+    )
+
+    let c3 = new Conta(
+        11,
+        3,
+        3000,
+        3,
+        titulares[2]
+    )
+
+    let contas = [c1, c2, c3]
+}
+
+
+
+
+export { Conta, buscarcontas };
